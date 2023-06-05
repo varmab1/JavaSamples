e1b5ad8d59f02bd443e719092b0839a23f531ed6
varmab1 <135039264+varmab1@users.noreply.github.com>
2023-06-05 10:54:41 -0500

    Delete assignment4 directory

--- assignment4/Interface1.java
+++
@@ -1,7 +0,0 @@
-package com.assignment4;
-
-public interface Interface1 {
-    void method1();
-
-
-}
--- assignment4/Interface2.java
+++
@@ -1,6 +0,0 @@
-package com.assignment4;
-
-public interface Interface2 {
-
-    void method2();
-}
